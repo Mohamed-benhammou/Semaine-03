d7a0596fc921c93e35ac95688a05b96f6dce1656
Mohamed <mohamed@siriusschool.be>
2020-06-24 13:09:02 +0200

    Upgrade Js

--- Exercices/exercices-javascript/1-les-boites-de-dialogue/script.js
+++ Exercices/exercices-javascript/1-les-boites-de-dialogue/script.js
@@ -1,5 +1,17 @@
-Exercice 1: affiche Hello World dans une boite de dialogue alert();
+/* Exercice 1: affiche Hello World dans une boite de dialogue
+window.alert('Hello World'); */
 
-// Exercice 2: Place ton code précédent en commentaire, dans un prompt, demande son prénom à l'utilisateur, ensuite, à l'aide d'un alert() affiche "Salut" + le prénom de l'utilisateur
+/*Exercice 2: Place ton code précédent en commentaire, dans un prompt, demande son prénom à l'utilisateur, ensuite, à l'aide d'un alert() affiche "Salut" + le prénom de l'utilisateur */
+/* const whatsYourName = prompt('Comment vous vous appelez ?');
+console.log(whatsYourName);
+alert(`Salut ${whatsYourName}`); */
 
-// Exercice 3: Place ton code précédent en commentaire. Dans un prompt, demande son âge à l'utilisateur, ensuite, à l'aide d'un .confirm() vérifie si l'âge de l'utilisateur est bien supérieur à 18 ans, si oui, demande à l'utilisateur de confirmer son âge et affiche "Bienvenue" dans une alert()
+/* Exercice 3: Place ton code précédent en commentaire. Dans un prompt, demande son âge à l'utilisateur, ensuite, à l'aide d'un .confirm() vérifie si l'âge de l'utilisateur est bien supérieur à 18 ans, si oui, demande à l'utilisateur de confirmer son âge et affiche "Bienvenue" dans une alert() */
+const age = prompt('Quel age avez vous ?');
+if (age > 18) {
+  const isMajor = confirm('Etes vous Majeurs ?');
+  if (isMajor) {
+    alert('Bienvenue');
+  }
+}
+console.log();
--- Exercices/exercices-javascript/2-la-calculatrice/script.js
+++ Exercices/exercices-javascript/2-la-calculatrice/script.js
@@ -1,23 +1,42 @@
-//Crée une fonction calculatrice: A l'aide des attributs name des éléments HTML input, récupère la valeur entrée par l'utilisateur dans ces inputs à l'aide de la méthode .value;  Ensuite, à l'aide des id des éléments button, effectue l'opération se rapportant au signe du bouton sur les valeurs récupérées des input. Pour terminer, affiche le résultat dans une boite de dialogue alert(). 
+//Crée une fonction calculatrice: A l'aide des attributs name des éléments HTML input, récupère la valeur entrée par l'utilisateur dans ces inputs à l'aide de la méthode .value;  Ensuite, à l'aide des id des éléments button, effectue l'opération se rapportant au signe du bouton sur les valeurs récupérées des input. Pour terminer, affiche le résultat dans une boite de dialogue alert().
 //Utilisez le bout de code suivant pour ajouter un évennement sur chaque bouton en remplacant le paramètre de getElementById par l'ID correspondant au bouton
 
 function additionOnClick() {
-    document.getElementById("").addEventListener("click", function addition() {
-        // Insérez ici le bout de code nécessaire à la réalisation de l'exercice
-
-})};
+  document
+    .getElementById('addition')
+    .addEventListener('click', function addition() {
+      const firstNumberValue = document.getElementById('first-number').value;
+      const secondNumberValue = document.getElementById('second-number').value;
+      alert(parseInt(firstNumberValue, 10) + parseInt(secondNumberValue, 10));
+    });
+}
 
 function substractionOnClick() {
-    document.getElementById("").addEventListener("click", function substract() {
-        // Insérez ici le bout de code nécessaire à la réalisation de l'exercice 
-})};
+  document
+    .getElementById('soustraction')
+    .addEventListener('click', function substract() {
+      const firstNumberValue = document.getElementById('first-number').value;
+      const secondNumberValue = document.getElementById('second-number').value;
+      alert(parseInt(firstNumberValue, 10) - parseInt(secondNumberValue, 10));
+    });
+}
 
 function divisionOnClick() {
-    document.getElementById("").addEventListener("click", function division() {
-        // Insérez ici le bout de code nécessaire à la réalisation de l'exercice 
-})};
+  document
+    .getElementById('division')
+    .addEventListener('click', function division() {
+      const firstNumberValue = document.getElementById('first-number').value;
+      const secondNumberValue = document.getElementById('second-number').value;
+      alert(parseInt(firstNumberValue, 10) / parseInt(secondNumberValue, 10));
+    });
+}
 
 function multiplicationOnClick() {
-    document.getElementById("").addEventListener("click", function multiplication() {
-        // Insérez ici le bout de code nécessaire à la réalisation de l'exercice 
-})};
+  document
+    .getElementById('multiplication')
+    .addEventListener('click', function multiplication() {
+      const firstNumberValue = document.getElementById('first-number').value;
+      const secondNumberValue = document.getElementById('second-number').value;
+      alert(parseInt(firstNumberValue, 10) * parseInt(secondNumberValue, 10));
+    });
+}
